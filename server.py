b12079db0bb486aac5c3e96638315a055f135fee
Mish Lee <micheleleee@gmail.com>
2015-06-13 13:34:52 -0700

    created base backend and html files

---
+++ server.py
@@ -0,0 +1,26 @@
+
+from flask import Flask, render_template, redirect, request, flash, session, jsonify, json
+from jinja2 import StrictUndefined
+from flask_debugtoolbar import DebugToolbarExtension
+
+
+app = Flask(__name__)
+
+app.secret_key = "ABC"
+
+app.jinja_env.undefined = StrictUndefined
+
+@app.route('/flight-detail')
+def get_city_pics():
+get_pics = requests.get('https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=536176452.1fb234f.f67ad8054ffe4f46aecff1b2e4c4b7c6')
+print get_pics
+return "hello"
+
+
+if __name__ == "__main__":
+app.debug = False
+
+DebugToolbarExtension(app)
+
+
+app.run()
