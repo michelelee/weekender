5831f036be05a17f36d1d7a470b8caff83cb7373
Forrest Lin <forrest@Forrests-MacBook-Air.local>
2015-06-13 17:22:05 -0700

    added keys

--- server.py
+++ server.py
@@ -1,7 +1,8 @@
-
 from flask import Flask, render_template, redirect, request, flash, session, jsonify, json
 from jinja2 import StrictUndefined
 from flask_debugtoolbar import DebugToolbarExtension
+from keys import amadeus_token
+from keys import instagram_token
 import requests
 
 app = Flask(__name__)
@@ -13,16 +14,18 @@ app.jinja_env.undefined = StrictUndefined
 @app.route('/')
 def go_home():
 	"""Homepage."""
-	print "lots of changes again"
 	return render_template("base.html")
 
 
 @app.route('/my-flight')
 def get_city_pics():
-	get_pics = requests.get('https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token=536176452.1fb234f.f67ad8054ffe4f46aecff1b2e4c4b7c6')
+	get_pics = requests.get('https://api.instagram.com/v1/media/search?lat=48.858844&lng=2.294351&access_token={token}'.format(token=instagram_token))
 	print get_pics
 	pics = get_pics
 	return render_template("/my_flight.html", pics=pics)
+
+def get_flights_list():
+	pass
 
 
 if __name__ == "__main__":
