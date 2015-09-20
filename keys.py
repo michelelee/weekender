34fcd42aa7ccc0caf1fedb1e1e9f6db07f4ca1a4
Michele Lee <Mish@Micheles-MacBook-Pro.local>
2015-09-20 13:19:51 -0700

    updated key for sabre api

--- keys.py
+++ keys.py
@@ -1,5 +1,7 @@
 instagram_token = '536176452.1fb234f.f67ad8054ffe4f46aecff1b2e4c4b7c6'
 
-amadeus_token = 'yuqY3bfBfuLkiAEjuXskO8GHGvu0xJPb'
+amadeus_token = 'yuqY3bfBfuLkiAEjuXskO8GHGvu0xJPb' 
 
-sabre_token = 'Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-0123456789012345678!123456!0!ABCDEFGHIJKLM!E2E-1'
+sabre_token = 'Bearer T1RLAQKQSwwsEB3GVPQhGDkLBp67dhaQ+hCKL95MWrsDln2QIVp+P9EiAACgy+P6p2cmS9arAPeeHQabfbU6LSQNgsv6dsyUXucj+JHSHuSV8QzHGouRocAayqjDpPxxG8DdHTnOpO2dgpO5mZway+sOEGtg4DKTldr1z0mle/eyO+9qFv2zbpC9hFuKqz52HYi08qYULVjRvHK7mhTq7XdhzqKlJlUiEs71kqlG+5H9Zp18FLQHyaGLsm63A0I4YVnGSbIcPK9oJfHu5w**'
+
+# 'Bearer Shared/IDL:IceSess\/SessMgr:1\.0.IDL/Common/!ICESMS\/ACPCRTD!ICESMSLB\/CRT.LB!-0123456789012345678!123456!0!ABCDEFGHIJKLM!E2E-1'
